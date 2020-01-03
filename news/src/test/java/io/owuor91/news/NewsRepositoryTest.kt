9ff2d6106d2b7ddf83cf0aed1114df7afd12e0d2
johnowuor <john.owuor@twigafoods.com>
2020-01-03 11:34:02 +0300

    add news module tests

---
+++ news/src/test/java/io/owuor91/news/NewsRepositoryTest.kt
@@ -0,0 +1,33 @@
+package io.owuor91.news
+
+import io.mockk.coEvery
+import io.mockk.impl.annotations.MockK
+import io.owuor91.mvvmnotesapp.models.Article
+import io.owuor91.mvvmnotesapp.models.NewsApiResponse
+import io.owuor91.news.repository.NewsRepository
+import junit.framework.Assert.assertEquals
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import org.junit.Test
+import retrofit2.Response
+
+class NewsRepositoryTest : BaseTest() {
+
+    @MockK
+    lateinit var newsRepository: NewsRepository
+
+    @Test
+    fun `test getTopHeadLines`() {
+        val newsApiResponse = NewsApiResponse("status", 10, ArrayList<Article>())
+        coEvery { newsRepository.getTopHeadlines("abcd", "1234") } returns Response.success(
+            newsApiResponse
+        )
+
+        GlobalScope.launch(Dispatchers.IO) {
+            assertEquals(newsRepository.getTopHeadlines("abcd", "1234"), newsApiResponse)
+        }
+    }
+
+
+}
---
+++ news/src/test/java/io/owuor91/news/NewsViewModelTest.kt
@@ -0,0 +1,52 @@
+package io.owuor91.news
+
+import io.mockk.coEvery
+import io.mockk.impl.annotations.InjectMockKs
+import io.mockk.impl.annotations.MockK
+import io.owuor91.mvvmnotesapp.models.Article
+import io.owuor91.mvvmnotesapp.models.NewsApiResponse
+import io.owuor91.news.repository.NewsRepository
+import io.owuor91.news.viewmodel.NewsViewModel
+import junit.framework.Assert.assertEquals
+import okhttp3.MediaType
+import okhttp3.ResponseBody
+import org.junit.Test
+import retrofit2.Response
+
+class NewsViewModelTest : BaseTest() {
+    @MockK
+    lateinit var newsRepository: NewsRepository
+
+    @InjectMockKs
+    lateinit var newsViewModel: NewsViewModel
+
+    @Test
+    fun `fetch news`() {
+        val newsApiResponse = NewsApiResponse("status", 10, ArrayList<Article>())
+
+        coEvery { newsRepository.getTopHeadlines("abcd", "1234") } returns Response.success(
+            newsApiResponse
+        )
+
+        newsViewModel.getNewsArticles("abcd", "1234")
+
+        val response = newsViewModel.getNewsResponse()
+
+        assertEquals(response.value, newsApiResponse.articles)
+    }
+
+    @Test
+    fun `fetch news error`() {
+        coEvery { newsRepository.getTopHeadlines("abcd", "1234") } returns
+                Response.error(
+                    401,
+                    ResponseBody.create(MediaType.parse("application/json"), "Not Authorized")
+                )
+
+        newsViewModel.getNewsArticles("abcd", "1234")
+
+        val error = newsViewModel.getNewsError()
+
+        assertEquals(error.value?.isEmpty() as Boolean, false)
+    }
+}
